2f4541e38e1e46e1a5766d01f806451251ba28cd
david smooke <45616838+davidsmooke@users.noreply.github.com>
2020-04-06 12:50:07 -0600

    Update AddCommentViewController.h

--- ViewController/AddCommentViewController.h
+++ ViewController/AddCommentViewController.h
@@ -2,9 +2,6 @@
 //  AddCommentViewController.h
 //  ArtMap
 //
-//  Created by sathish kumar on 10/29/12.
-//  Copyright (c) 2012 sathish kumar. All rights reserved.
-//
 
 #import <UIKit/UIKit.h>
 #import "Config.h"
